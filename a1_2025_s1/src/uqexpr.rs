0836ff69a7c4262d8d5ae499f9463134e27b55ad
yaojun <940334249@qq.com>
2025-12-14 17:36:14 +0800

    feat: pass the test 1 for a1_2025_s1

--- a1_2025_s1/src/uqexpr.rs
+++ a1_2025_s1/src/uqexpr.rs
@@ -32,7 +32,7 @@ struct Config{
 
 fn handle_command_line_arguments()->Result<Config, ExitError>{
      let args: Vec<String> = std::env::args().collect();
-    let config = Config{
+    let mut config = Config{
         init_string: String::from(""),
         significant_figures: 0,
         for_loop: String::from(""),
@@ -42,13 +42,14 @@ fn handle_command_line_arguments()->Result<Config, ExitError>{
     let mut init_flag = false;
     let mut figure_flag = false;
     let mut forloop_flag = false;
+    let mut filename_flag = false;
    while i < args.len(){
        if args[i].is_empty(){
            return Err(ExitError::Usage);
        }
        match args[i].as_str(){
            "--init"=>{
-                    if i+1 < args.len(){
+                    if i+1 >= args.len(){
                         return Err(ExitError::Usage);
                     }
                    if init_flag{
@@ -58,17 +59,34 @@ fn handle_command_line_arguments()->Result<Config, ExitError>{
                i += 1;
            }
            "--significantfigures"=>{
-               if i+1 < args.len(){
+               if i+1 >= args.len(){
                         return Err(ExitError::Usage);
                     }
                    if figure_flag{
                    return Err(ExitError::Usage);
                }
+                if(has_leading_zero(&args[i+1].as_str())){
+                    return Err(ExitError::Usage);
+                }
                figure_flag = true;
+               let parse_int = args[i+1].parse::<u8>();
+               match parse_int {
+                   Err(e)=>{
+                       return Err(ExitError::Usage);
+                   }
+                   Ok(x)=>{
+                       if x >= 2 && x <= 8{
+                           config.significant_figures = x;
+                       }else{
+                           return Err(ExitError::Usage);
+                       }
+                   }
+
+               }
                i += 1;
            }
            "--forloop"=>{
-               if i+1 < args.len(){
+               if i+1 >= args.len(){
                         return Err(ExitError::Usage);
                     }
                    if forloop_flag{
@@ -78,7 +96,13 @@ fn handle_command_line_arguments()->Result<Config, ExitError>{
                i += 1;
            }
            _=>{
-
+               if args[i].starts_with("--"){
+                   return Err(ExitError::Usage);
+               }
+               if filename_flag {
+                   return Err(ExitError::Usage);
+               }
+               filename_flag = true;
                i+=1;
            }
        }
@@ -87,7 +111,20 @@ fn handle_command_line_arguments()->Result<Config, ExitError>{
     Ok(config)
 }
 
-fn check_input_filename(filename: &String)->Result<Vec<String>, ExitError>{
+fn has_leading_zero(s:&str) -> bool {
+    if s.len()  <= 1{
+        return false;
+    }
+    // Rust中字符串访问每个元素，需要转换为 Vec之后访问
+    let chars: Vec<char> = s.chars().collect();
+
+    if chars[0] == '0' {
+        return true;
+    }
+    false
+}
+
+fn check_input_filename(filename: &String) ->Result<Vec<String>, ExitError>{
 
     let file = File::open(filename);
     if file.is_err(){
